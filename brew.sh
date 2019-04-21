5acf659d740adde701671925aa1663d9cbc773f6
faraz <95059+faraz@users.noreply.github.com>
2019-04-21 18:18:50 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -140,5 +140,8 @@ brew install httpie
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip qlimagesize webpquicklook suspicious-package quicklookase qlvideo jq
 
+brew install packer
+brew install terraform
+
 # Remove outdated versions from the cellar.
 brew cleanup
